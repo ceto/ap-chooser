99fce82f3f498bc460f5596982098b9cc6616d61
Gabor Szabo <szabogabi@gmail.com>
2016-06-02 22:39:25 +0200

    smart ap lists

--- templates/partials/aprow.php
+++ templates/partials/aprow.php
@@ -1,3 +1,24 @@
-<div class="apc-aprow">
-  <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-</div>
+<p class="datarow">
+  <a id="ap-<?= get_the_ID();  ?>" class="datarow--link statsuz-<?= get_field('statusz'); ?>" href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"
+  data-name="<?php the_title(); ?>"
+  data-alapterulet="<?= get_field('alapterulet') ?>"
+  data-etkezo="<?= get_field('etkezo') ?>"
+  data-konyha="<?= get_field('konyha') ?>"
+  data-szobak="<?= count(get_field('szobak')) ?>"
+  data-pris="<?= get_field('pris') ?>"
+  data-statsuz="<?= get_field('statusz') ?>"
+  data-svgdata="<?= get_field('svg_data') ?>"
+  data-url="<?php the_permalink(); ?>"
+  >
+    <span class="datarow--cell"><?php the_title(); ?></span>
+    <span class="datarow--cell"><?= get_field('alapterulet') ?>m<sup>2</sup></span>
+    <span class="datarow--cell"><?= count( get_field('szobak')) ?></span>
+    <span class="datarow--cell"><?= get_field('konyha') ?>m<sup>2</sup></span>
+    <span class="datarow--cell"><?= get_field('etkezo') ?>m<sup>2</sup></span>
+    <?php if ( get_field('statsuz') === 'free' ) : ?>
+      <span class="datarow--cell"><i class="<?= get_field('statusz') ?>"></i><?= get_field('pris') ?></span>
+    <?php else : ?>
+      <span class="datarow--cell"><i class="<?= get_field('statusz') ?>"></i><?= get_field('statusz') ?></span>
+    <?php endif; ?>
+  </a>
+</p>
--- templates/taxonomy-blokk.php
+++ templates/taxonomy-blokk.php
@@ -14,43 +14,80 @@
   }
 ?>
 <main class='apc-ns' role="main">
-  <header class="apc-pageheader">
-    <div class="row">
-      <div class="columns">
-        <ul class="breadcrumbs">
-          <li><a href="<?= get_term_link('deltaliget','blokk') ?>">Deltaliget</a></li>
-        </ul>
-        <h1 class='apc-pageheader__title'><?= $blokktype!='root'? $parentblokk->name.' - ':'' ?><?= $aktblokk->name; ?></h1>
+  <section class="choserblock" id="finndinbolig">
+    <div class="thechooser">
+      <?php
+        $nezeti_kep = get_field('nezeti_kep', $aktblokk);
+        if ( !empty($nezeti_kep) ): ?>
+          <div id="visualchooser" class="visualchooser" data-width="2600" data-height="1569">
+            <?= wp_get_attachment_image( $nezeti_kep['id'], 'full' ); ?>
+          </div>
+      <?php endif;?>
+
+      <header class="apc-pageheader">
+        <div class="row">
+          <div class="columns">
+            <?php if ($blokktype!='root') : ?>
+              <ul class="breadcrumbs">
+                <li><a href="<?= get_term_link('deltaliget','blokk') ?>">Deltaliget</a></li>
+                <?php if ($blokktype=='floor') : ?>
+                <li><a href="<?= get_term_link($parentblokk );  ?>"><?php echo $parentblokk->name;?></a></li>
+                <?php endif; ?>
+              </ul>
+            <?php endif; ?>
+
+            <h1 class='apc-pageheader__title'><?= $aktblokk->name; ?></h1>
+          </div>
+        </div>
+      </header>
+
+      <div class="row">
+        <div class="columns">
+
+          <?php
+            if ($blokktype == 'floor') {
+              $terms = array($aktblokk);
+            } else {
+              $terms = get_terms('blokk', array(
+               'parent' => $aktblokk->term_id,
+               'orderby' => 'slug'
+              ));
+            }
+
+
+          foreach( $terms as $term ):  ?>
+            <?php if ($blokktype!='floor') : ?>
+            <h2><a href="<?= get_term_link($term );  ?>"><?php echo $term->name;?></a></h2>
+            <?php endif; ?>
+            <div class="datatable">
+              <p class="datarow datatable--head">
+                <span class="datarow--cell">Lakás</span>
+                <span class="datarow--cell">Alapterület</span>
+                <span class="datarow--cell">Szobák</span>
+                <span class="datarow--cell">Konyha</span>
+                <span class="datarow--cell">Étekző</span>
+                <span class="datarow--cell">Ár / Állapot</span>
+              </p>
+              <?php
+                  $posts = get_posts(array(
+                    'post_type' => 'lakas',
+                    'taxonomy' => $term->taxonomy,
+                    'term' => $term->slug,
+                    'nopaging' => true, // to show all posts in this taxonomy, could also use 'numberposts' => -1 instead
+                    'orderby' => 'title',
+                    'order' => 'ASC'
+                  ));
+                  foreach($posts as $post): // begin cycle through posts of this taxonmy
+                    setup_postdata($post); //set up post data for use in the loop (enables the_title(), etc without specifying a post ID)
+                  ?>
+                    <?php require('partials/aprow.php'); ?>
+                  <?php endforeach; ?>
+              </div>
+          <?php endforeach; ?>
+        </div>
       </div>
     </div>
-  </header>
-  <div class="row">
-    <div class="columns">
-      <?php // Output all Taxonomies names with their respective items
-        $terms = get_terms('blokk', array(
-         'parent' => $aktblokk->term_id,
-         'orderby' => 'slug'
-        ));
-        foreach( $terms as $term ):  ?>
-          <h2><a href="<?= get_term_link($term );  ?>"><?php echo $term->name;?></a></h2>
-          <?php
-              $posts = get_posts(array(
-                'post_type' => 'lakas',
-                'taxonomy' => $term->taxonomy,
-                'term' => $term->slug,
-                'nopaging' => true, // to show all posts in this taxonomy, could also use 'numberposts' => -1 instead
-                'orderby' => 'title',
-                'order' => 'ASC'
-              ));
-              foreach($posts as $post): // begin cycle through posts of this taxonmy
-                setup_postdata($post); //set up post data for use in the loop (enables the_title(), etc without specifying a post ID)
-              ?>
-                <?php require('partials/aprow.php'); ?>
-              <?php endforeach; ?>
-              <hr>
-        <?php endforeach; ?>
-    </div>
-  </div>
+  </section>
 </main>
 
 
